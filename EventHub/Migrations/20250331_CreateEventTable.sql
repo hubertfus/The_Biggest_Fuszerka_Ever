ad9b85231cdb1ae78f2ef74819f51c1325521e23
Hubert Fusiarz <hubertfusiarz@gmail.com>
2025-04-02 00:12:50 +0200

    .

--- EventHub/Migrations/20250331_CreateEventTable.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE events (
-                        id SERIAL PRIMARY KEY,
-                        name VARCHAR(255) NOT NULL,
-                        date TIMESTAMP,
-                        description TEXT,
-                        imageurl VARCHAR(255),
-                        organizerid INTEGER,
-                        FOREIGN KEY (organizerid) REFERENCES organizers(id)
-);
--- EventHub/Migrations/20250331_CreateOrganizerTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE organizers (
-                            id SERIAL PRIMARY KEY,
-                            name VARCHAR(255),
-                            description TEXT,
-                            email VARCHAR(255),
-                            logourl VARCHAR(255)
-);
